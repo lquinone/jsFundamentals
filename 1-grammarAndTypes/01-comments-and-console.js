c6009b69bc70303a107e02634de7b65ea941fc3c
Lizeth Quinones G <lizeth@Lizeths-MacBook-Pro.local>
2021-08-19 17:52:05 -0400

    Firstcommit

---
+++ 1-grammarAndTypes/01-comments-and-console.js
@@ -0,0 +1,23 @@
+/*
+COMMETS
+
+1-grammarAndTypes
+    01-comments-and-console.js
+*/
+
+//value of a comment:
+//communicates to anyone reading our code
+
+//types of comments:
+//1) single-line comment
+
+/*
+2) multi-line comments
+*/
+
+//console.log
+//outputs some message to the console
+console.log('this is a console.log message!');
+
+console.log('you can separate parts of a consle message with commas', ',just like this!');
+
